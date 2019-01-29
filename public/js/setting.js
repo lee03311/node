2fb365576c3d62ed7eed8facb11e5f62731cef98
slee <lee03311@naver.com>
2019-01-29 23:12:30 +0900

    190129

--- public/js/setting.js
+++ public/js/setting.js
@@ -15,7 +15,19 @@ function getCategoryList(){
                 var categoryList = $("#categoryList");
                 
                 for(var i=0;i<category.length;i++){
-                    $("<span/>").text(category[i].category).appendTo(categoryList);
+                    // $("<span/>").text(category[i].category).appendTo(categoryList);
+
+                    $('<div/>').append(
+                            $('<div/>').addClass('categoryArea').css('background-color', category[i].color)
+                        ).append(
+                            $('<span/>').addClass('categoryText').text(category[i].category)
+                        ).append(
+                            $('<label/>').addClass('switch').append(
+                                $('<input/>').attr('type', 'checkbox')
+                            ).append(
+                                $('<span/>').addClass('slider round')
+                            )
+                        ).appendTo(categoryList);
                 }
             }
         }
